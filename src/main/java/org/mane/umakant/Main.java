2bea793d3195bc5e25b7805f5e7d9c28ba00b98b
umakantmane22 <umakantmane22@gmail.com>
2024-09-09 23:16:31 +0530

    abc gh

--- src/main/java/org/mane/umakant/Main.java
+++ src/main/java/org/mane/umakant/Main.java
@@ -23,6 +23,8 @@ public class Main {
         System.out.println("4");
         System.out.println("5");
         System.out.println("F");
+        System.out.println("G");
+        System.out.println("H");
          System.out.println("abc");
     }
 }
